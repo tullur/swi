8d9b5b76d2d7aded4c4d99a1bbb4cea1a5283b6c
psidae <akson.rill@gmail.com>
2019-05-06 19:50:37 +0200

    Update

--- source/util/write.go
+++ source/util/write.go
@@ -4,17 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"os"
-	"strconv"
-	"strings"
 )
-
-func _UnescapeUnicodeCharactersInJSON(_jsonRaw json.RawMessage) (json.RawMessage, error) {
-	str, err := strconv.Unquote(strings.Replace(strconv.Quote(string(_jsonRaw)), `\\u`, `\u`, -1))
-	if err != nil {
-		return nil, err
-	}
-	return []byte(str), nil
-}
 
 // WriteJSON write unnmarshaled xml data to the JSON file
 func WriteJSON(name string, data map[string]map[string]interface{}) {
