7ec60df74d458373543328a68e976146c431a831
psidae <akson.rill@gmail.com>
2019-05-06 19:50:22 +0200

    Add safe encoding

--- source/util/write.go
+++ source/util/write.go
@@ -1,9 +1,20 @@
 package util
 
 import (
+	"bytes"
 	"encoding/json"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func _UnescapeUnicodeCharactersInJSON(_jsonRaw json.RawMessage) (json.RawMessage, error) {
+	str, err := strconv.Unquote(strings.Replace(strconv.Quote(string(_jsonRaw)), `\\u`, `\u`, -1))
+	if err != nil {
+		return nil, err
+	}
+	return []byte(str), nil
+}
 
 // WriteJSON write unnmarshaled xml data to the JSON file
 func WriteJSON(name string, data map[string]map[string]interface{}) {
@@ -11,6 +22,10 @@ func WriteJSON(name string, data map[string]map[string]interface{}) {
 	CheckError(err)
 
 	result, err := json.MarshalIndent(data, "", "\t")
+	result = bytes.Replace(result, []byte("\\u003c"), []byte("<"), -1)
+	result = bytes.Replace(result, []byte("\\u003e"), []byte(">"), -1)
+	result = bytes.Replace(result, []byte("\\u0026"), []byte("&"), -1)
+
 	CheckError(err)
 	f.Write(result)
 }
